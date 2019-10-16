1c09fa54f5670f7b71b2a8765901dd71c52fc8c1
marcegarzab <marcegarzab@gmail.com>
2019-10-15 19:29:39 -0500

    final changes

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -12,8 +12,12 @@ const longAndLatFunc = function(cityName, callback){ //mapbox api for lat and lo
 			if(data.Response == 'False'){
 				callback(data.Error, undefined)
 			}else{
-				const longAndLat = data.features[0].center
-				callback(undefined, longAndLat)
+				const info = {
+					longAndLat: data.features[0].center,
+					cityName: cityName
+				}
+				// const longAndLat = data.features[0].center
+				callback(undefined, info)
 			}
 		}
 	})
@@ -24,10 +28,10 @@ const climateFunc = function(cityName, longAndLat, callback){ //darksky api for 
 	// 	console.log("entro")
 	// 	return 1;
 	// }
-	const LongLatString = longAndLat.toString()
-	const splitLongLat = LongLatString.split(",")
-	const lat = parseFloat(splitLongLat[1])
-	const long = parseFloat(splitLongLat[0])
+	// const LongLatString = longAndLat.toString()
+	// const splitLongLat = LongLatString.split(",")
+	const lat = parseFloat(longAndLat[1])
+	const long = parseFloat(longAndLat[0])
 
 	const url = 'https://api.darksky.net/forecast/' + credentials.DARK_SKY_SECRET_KEY + '/' + lat + "," + long + "?lang=es&units=si"
 	request({url, json:true}, function(error, response){
@@ -43,7 +47,7 @@ const climateFunc = function(cityName, longAndLat, callback){ //darksky api for 
 					temp : data.currently.temperature,
 					precip : data.currently.precipProbability
 				}
-				callback(climate)
+				callback(undefined, climate)
 			}
 			// console.log(cityName + ": " + climate.description + " Actualmente esta a " + climate.temp + "°C. Hay " + (climate.precip*100) + "% de posibilidad de lluvia.")		
 		}
